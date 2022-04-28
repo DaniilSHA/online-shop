2d16a7877a4612f1c87aba9e5cce712e4b8c36d8
Daniil_Shakhmovedev <Daniil_Shakhmovedev@epam.com>
2022-04-28 22:47:42 +0300

    make auth service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,8 @@ import {AppRoutingModule} from "./app-routing.module";
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
 import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -17,10 +19,12 @@ import { RegisterPageComponent } from './register-page/register-page.component';
     RegisterPageComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
