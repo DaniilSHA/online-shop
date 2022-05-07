438937f77da5213a88a24a051f2b46d06c862e53
Daniil_Shakhmovedev <Daniil_Shakhmovedev@epam.com>
2022-05-07 17:19:02 +0300

    make tamplate v2

--- client/src/app/product-card/product-card.component.ts
+++ client/src/app/product-card/product-card.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.css']
+  styleUrls: ['./product-card.component.css'],
+  inputs: ['title', 'price', 'img']
 })
 export class ProductCardComponent implements OnInit {
+
+  title: string = '';
+  img: string = '';
+  price: number = 0;
 
   constructor() { }
 
