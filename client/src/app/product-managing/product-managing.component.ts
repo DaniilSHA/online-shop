320cb4fe4f3dc35c802f1d116eba4a37b3467015
Daniil_Shakhmovedev <Daniil_Shakhmovedev@epam.com>
2022-05-10 22:52:25 +0300

    make products request v3

--- client/src/app/product-managing/product-managing.component.ts
+++ client/src/app/product-managing/product-managing.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, SecurityContext} from '@angular/core';
 import {ProductService} from "../shared/services/product.service";
 import {Product} from "../shared/interfaces";
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-product-managing',
@@ -10,8 +11,10 @@ import {Product} from "../shared/interfaces";
 export class ProductManagingComponent implements OnInit {
 
   products!: [Product];
+  imagePath!: string;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private sanitizer : DomSanitizer) {}
 
   ngOnInit(): void {
     this.reloadProducts();
@@ -20,7 +23,20 @@ export class ProductManagingComponent implements OnInit {
   private reloadProducts() : void {
     this.productService.getProductList().subscribe(data => this.products = data)
 
-    // @ts-ignore
-    setTimeout(()=>console.log(this.products[5].img), 1000)
+    // // @ts-ignore
+    // setTimeout(()=>console.log(this.products[8].img), 1000)
+    // // @ts-ignore
+    // setTimeout(()=>console.log(this.products[8].id), 1000)
+
+
+    setTimeout(()=> {
+      // @ts-ignore
+      this.imagePath = this.sanitizer.sanitize(SecurityContext.RESOURCE_URL, this.sanitizer.bypassSecurityTrustResourceUrl('data:image/jpg;base64,' + this.products[8].img));
+      // @ts-ignore
+      // console.log(this.products[8].img)
+      console.log(this.imagePath)
+    }, 1000)
+
+
   }
 }
